5459824a9b95cbc1bc738ceac2dbcba11e53e42e
JosephAtAlitech <joseph@alitechbd.com>
2023-12-21 20:01:40 +0600

    commit

--- config.php
+++ config.php
@@ -1,14 +1,20 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=vftracke_track_uat", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+ // echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
+$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$conn->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+define('APP_NAME', "VFTracker Bkash Bill Payment")
+
 ?>
